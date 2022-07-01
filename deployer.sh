afb2674fb3b62a6369a619196c78d57e96ce5b6a
Emm Kwami <edenated@gmail.com>
2022-07-01 12:16:35 -0700

    deployer script worked

--- deployer.sh
+++ deployer.sh
@@ -7,7 +7,7 @@ set -e
 echo "Deploying application ..."
 
 # Enter maintenance mode
-(php artisan down --message 'The app is being (quickly!) updated. Please try again in a minute.') || true
+(php artisan down ) || true
     # Update codebase
     git pull origin main
 # Exit maintenance mode
